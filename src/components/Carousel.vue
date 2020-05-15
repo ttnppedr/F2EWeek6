ca8c2b2654a3a89f2ce34653e7f80c4a9169582a
Penghua <kids5346@gmail.com>
2020-05-16 00:39:36 +0800

    [feat]: use vuetify to  build Carousel component

---
+++ src/components/Carousel.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  div
+    v-app(id="inspire")
+      v-carousel(
+        hide-delimiter-background
+        show-arrows-on-hover
+      )
+        v-carousel-item(
+          v-for="(item,i) in items"
+          :key="i"
+          :src="item.src"
+          reverse-transition="fade-transition"
+          transition="fade-transition"
+        )
+</template>
+
+<script>
+
+export default {
+  props: {},
+  data() {
+    return {
+      items: [
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+        },
+        {
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+        },
+      ],
+    }
+  },
+  methods: {},
+  computed: {},
+  components: {},
+  mounted() { }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/vendors/vuetify/carousel.scss';
+</style>
