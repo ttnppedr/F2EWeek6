3a81f318e2ed19559c48c1fe2b7e4ac6be1df8ae
Penghua <kids5346@gmail.com>
2020-05-16 14:04:17 +0800

    [feat]: singleRoomImgs to get each single room images

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,10 +1,18 @@
 export default {
-  formatAllRooms: state => {
-    return state.rooms.map(room => {
+  formatAllRooms: ({ rooms }) => {
+    return rooms.map(room => {
       return {
         id: room.id,
         img: room.imageUrl
       }
     })
+  },
+  singleRoomImgs: ({ singleRoom }) => {
+    const initObj = [
+      { imageUrl: [] }
+    ];
+    return singleRoom.room
+      ? singleRoom.room[0].imageUrl
+      : singleRoom.room = initObj; 
   }
 }
