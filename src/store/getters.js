e95661f83451a0b553fe63f4a3e0452cf9b6a0b2
Penghua <kids5346@gmail.com>
2020-05-18 19:56:06 +0800

    [feat]: all rooms descriptions

--- src/store/getters.js
+++ src/store/getters.js
@@ -16,6 +16,11 @@ export default {
   },
   roomSpecification: state => name => {
     const obj = {
+      "Single Room": "1人・ 單人床・ 附早餐・衛浴1間・18平方公尺",
+      "Double Room": "2~3人・ 雙人床・ 附早餐・衛浴1間・26平方公尺",
+      "Twin Room": "2~4人・ 雙人床・ 附早餐・衛浴1間・36平方公尺",
+      "Deluxe Twin Room": "2~4人・ 大型雙人床・ 附早餐・衛浴1間・40平方公尺",
+      "Deluxe Single Room": "1人・ 小型雙人床・ 附早餐・衛浴1間・22平方公尺",
       "Deluxe Double Room": "2~3人・ 雙人床・ 附早餐・衛浴1間・26平方公尺",
     };
     return obj[name];
