a80f0597c25bfaa7e98ebf12165db0b44869bbdc
Penghua <kids5346@gmail.com>
2020-05-18 15:35:46 +0800

    [feat]: calculate period of days

--- src/assets/utils/dateConvertor.js
+++ src/assets/utils/dateConvertor.js
@@ -5,3 +5,12 @@ export const calculateDays = num => {
   const sum = date.getTime() + TIMESTAMP * num;
   return new Date(sum);
 }
+
+export const periodOfDays = (firstDay, secondDay) => {
+  const TIMESTAMP = 86400000;
+  const firstTimestamp = new Date(firstDay).getTime();
+  const secondTimestamp = new Date(secondDay).getTime();
+  const period = (secondTimestamp - firstTimestamp) / TIMESTAMP;
+  return period;
+}
+
