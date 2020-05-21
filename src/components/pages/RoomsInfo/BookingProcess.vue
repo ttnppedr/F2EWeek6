978ada866ed5d2b0bb511366708e6ee8433b73e9
Penghua <kids5346@gmail.com>
2020-05-21 14:53:44 +0800

    [feat]: build BookingProcess

---
+++ src/components/pages/RoomsInfo/BookingProcess.vue
@@ -0,0 +1,66 @@
+<template lang="pug">
+  div.booking-process
+    div.process-img
+      slot(name="processImg")
+    div.process-description
+      slot(name="prcessDescription")
+</template>
+
+<script>
+// plugin
+import { mapActions, mapGetters } from "vuex";
+
+// components
+
+// views
+
+
+// assets
+
+export default {
+  props: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    ...mapActions({
+
+    })
+  },
+  computed: {
+    ...mapGetters({
+
+    })
+  },
+  components: {
+  },
+  mounted() {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .booking-process {
+    border: 1px solid #949C7C;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+    width: 300px;
+    .process-img {
+      background: #949C7C;
+      padding: 10px;
+      text-align: center;
+    }
+    .process-description {
+      height: 86px;
+      padding-top: 14px;
+      padding: 14px 5px;
+      text-align: center;
+      font-size: 14px;
+    }
+  }
+</style>
