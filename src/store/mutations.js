f989cc54526a0c12a9faf0a8b1fd60508fc0a2df
Penghua <kids5346@gmail.com>
2020-05-21 22:54:54 +0800

    [feat]: updateBookingRoomSuccess, updateBookingRoomFail

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -4,5 +4,13 @@ export default {
   },
   updateSingleRooms(state, payload) {
     state.singleRoom = payload;
-  } 
+  },
+  updateBookingRoomSucces(state, { success }) {
+    state.bookingStatus.success = success;
+    state.bookingStatus.fail = '';
+  },
+  upadteBookingRoomFail(state, { message }) {
+    state.bookingStatus.fail = message;
+    state.bookingStatus.success = false;
+  },
 }
